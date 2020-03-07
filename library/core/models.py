1701b0d4e35ec78238c329388e74bb29d46e2e16
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2020-03-07 13:47:17 -0300

    added app core in settings and fixtures

--- library/core/models.py
+++ library/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    author = models.CharField(max_length=255, null=False, blank=False)
+    cover = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
